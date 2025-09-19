3f4fbe4159b2f4202d0400de7072caaac2ed4e1a
Blootzz <106506598+Blootzz@users.noreply.github.com>
2025-09-18 22:14:28 -0500

    Added Main Menu

---
+++ Assets/Code/UI/Menus/Canvas_MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Canvas_MainMenu : MonoBehaviour
+{
+    public void OpenOptionsMenu()
+    {
+        print("Options button hit");
+    }
+
+    public void Quit()
+    {
+        print("Quit the game");
+    }
+}
